240ba324a572028c47275281471172fc325651f9
aktuba <aktuba@yandex.ru>
2022-12-20 14:02:04 +0300

    check overflow

--- tests/SumTest.php
+++ tests/SumTest.php
@@ -2,9 +2,9 @@
 
 namespace Nazububu\Sum\Tests;
 
-use PHPUnit\Framework\TestCase;
-
 use Nazububu\Sum\Sum;
+use Nazububu\Sum\SumException;
+use PHPUnit\Framework\TestCase;
 
 /**
  * Class SumTest
@@ -16,12 +16,45 @@ class SumTest extends TestCase
     /**
      * Test of adding two valid numbers
      *
+     * @dataProvider providerAddTwoNumbers
+     * @covers       Sum::calculate
+     *
+     * @param  int  $first
+     * @param  int  $second
+     * @param  int|null  $expect
+     * @param  bool  $expectException
      * @return void
+     * @throws SumException
      */
-    public function testAddTwoNumbers(): void
+    public function testAddTwoNumbers(int $first, int $second, ?int $expect, bool $expectException): void
     {
         $sum = new Sum;
 
-        $this->assertEquals(2, $sum->calculate(1, 1));
+        if ($expectException) {
+            $this->expectException(SumException::class);
+        }
+
+        $this->assertEquals($expect, $sum->calculate($first, $second));
+    }
+
+    /**
+     * @return array[]
+     */
+    public function providerAddTwoNumbers(): array
+    {
+        return [
+            'valid' => [
+                'first' => 1,
+                'second' => 2,
+                'expect' => 3,
+                'expectException' => false,
+            ],
+            'overflow' => [
+                'first' => 1,
+                'second' => PHP_INT_MAX,
+                'expect' => null,
+                'expectException' => true,
+            ],
+        ];
     }
 }
